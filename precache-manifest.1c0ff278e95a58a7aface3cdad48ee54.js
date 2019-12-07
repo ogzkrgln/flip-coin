0bd26671ad9285d45b5ce483256513fb1da03505
Orhan İnce <ince.orhan@yandex.com>
2019-12-07 15:18:37 +0300

    Updates

---
+++ precache-manifest.1c0ff278e95a58a7aface3cdad48ee54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38c1dc7101027131a28da2df8b3850b",
+    "url": "/flip-coin/index.html"
+  },
+  {
+    "revision": "334ef57f482182aca57e",
+    "url": "/flip-coin/static/css/main.590feecb.chunk.css"
+  },
+  {
+    "revision": "a52fa2e3d750a5559fc2",
+    "url": "/flip-coin/static/js/2.c202c282.chunk.js"
+  },
+  {
+    "revision": "334ef57f482182aca57e",
+    "url": "/flip-coin/static/js/main.f5f39ec5.chunk.js"
+  },
+  {
+    "revision": "5af720d5103d84af6da3",
+    "url": "/flip-coin/static/js/runtime~main.60953105.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/flip-coin/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/flip-coin/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
